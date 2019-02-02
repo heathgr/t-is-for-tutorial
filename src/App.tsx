77167d9aa0099eda79caabde6fb0d75ce00fcbe4
Grant Heath <heathgr@gmail.com>
2019-02-01 19:49:46 -0700

    The latest css tweaks plus code commnts.

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,33 @@
 import { StoreProvider } from '@s-is-for-store/react'
-import React, {StatelessComponent} from 'react';
+import React, {FunctionComponent} from 'react';
 import store, { AppState, Details, changeCity } from './store'
 
 import './App.css'
+
+const numberWithCommas = (x: number) => {
+  const parts = x.toString().split('.')
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+  return parts.join('.')
+}
+
+const Fact: FunctionComponent<{ label: string, value: string | null , isWorking: boolean }> = ({ label, isWorking, value}) => (
+  <div className='detail'>
+    <div className='label'>{label}</div>
+    <div className='value'>{(isWorking || !value) ? '---' : value}</div>
+  </div>
+)
 
 const App = () => (
   <StoreProvider<AppState> store={store}>
     {
       (state) => (
-        <div className='App'>
-          <h2>{`${state.cities[state.currentCity].name} Fact Sheet`}</h2>
+        <div className={`App ${state.isWorking ? 'working' : ''}`}>
+          <h2>{state.isWorking ? '---' : `${state.cities[state.currentCity].name} Fact Sheet`}</h2>
           <section>
-             <div className='detail'>
-              <div className='label'>Area</div>
-              <div className='value'>{`${state.details && state.details.area}km`}</div>
-             </div>
-             <div className='detail'>
-              <div className='label'>Founded</div>
-              <div className='value'>{state.details && state.details.founded}</div>
-             </div>
-             <div className='detail'>
-              <div className='label'>Population</div>
-              <div className='value'>{state.details && state.details.population}</div>
-             </div>
-             <div className='detail'>
-              <div className='label'>Time Zone</div>
-              <div className='value'>{state.details && state.details.timeZone}</div>
-             </div>
+            <Fact label='Area' value={`${state.details && numberWithCommas(state.details.area)}km`} isWorking={state.isWorking} />
+            <Fact label='Founded' value={state.details && state.details.founded.toString()} isWorking={state.isWorking} />
+            <Fact label='Population' value={state.details && numberWithCommas(state.details.population)} isWorking={state.isWorking} />
+            <Fact label='Time Zone' value={state.details && state.details.timeZone.toString()} isWorking={state.isWorking} />
           </section>
           <select disabled={state.isWorking} value={state.currentCity} onChange={(evt) => changeCity(Number(evt.currentTarget.value))}>
             {
--- src/store.ts
+++ src/store.ts
@@ -32,14 +32,25 @@ const initialState: AppState = {
   isWorking: true,
 }
 
+// Creates the store for the app state.
 const store = createStore<AppState>(initialState)
+// Exposes the store's resolve state function.
 const { resolveState } = store
 
+/**
+ * Sets the `isWorking` state property to true or false.
+ * Setting `isWorking` to true signifies that the app is in the middle of an http request.
+ * @param status True if the app is in the middle of an http request.  False if the app is not
+ */
 export const setIsWorking = (status: boolean) => resolveState((getState) => ({
   ...getState(),
   isWorking: status,
 }))
 
+/**
+ * Makes an HTTP request to fetch the available cities.
+ * Once the HTTP request is complete, the `cities` property is set to the result.
+ */
 export const getCities = () => resolveState(async (getState) => {
   const citiesRequest = await fetch('http://localhost:3001/cities?_sort=name')
   const cities = await citiesRequest.json() as City[]
@@ -50,11 +61,19 @@ export const getCities = () => resolveState(async (getState) => {
   }
 })
 
+/**
+ * Sets the `currentCity` state property.
+ * @param currentCity The index of the city that will be focused on.
+ */
 export const setCurrentCity = (currentCity: number) => resolveState((getState) => ({
   ...getState(),
   currentCity,
 }))
 
+/**
+ * Makes an http request to retrieve the details of the current city.
+ * The `details` state is set to the result of the request.
+ */
 export const getDetails = () => resolveState(async (getState) => {
   const { cities, currentCity: currentCityId } = getState()
   const currentCity = cities[currentCityId]
@@ -66,6 +85,10 @@ export const getDetails = () => resolveState(async (getState) => {
   }
 })
 
+/**
+ * Called when the app is initialized.
+ * It retrieves the available cities and gets the details of the first city.
+ */
 export const initializeApp = async () => {
   await setIsWorking(true)
   await getCities()
@@ -73,6 +96,10 @@ export const initializeApp = async () => {
   await setIsWorking(false)
 }
 
+/**
+ * Sets the current city and retrieves its details.
+ * @param newCityId The new current city.
+ */
 export const changeCity = async (newCityId: number) => {
   await setIsWorking(true)
   await setCurrentCity(newCityId)
